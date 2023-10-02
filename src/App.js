45c760fb0157753c4353cb61321e30be86fac118
llcain <llcain2012@gmail.com>
2023-10-01 21:13:20 -0500

    Created Home.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import '../src/App.scss';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className='content'>
-        <h1>Hello!</h1>
+        <Home />
       </div>
       
     </div>
