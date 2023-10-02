34aa61b40d007f8c2f2fb0baa7e03853a9964e40
llcain <llcain2012@gmail.com>
2023-10-01 21:49:48 -0500

    Mapping through blogs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'Create React App', body: 'Create a folder by opening bash terminal. Give the folder a name. Open in VS code. Open ternal and run npm create-react-app with dot afterwards. Press enter', author: 'laquisha', id: 1},
+        { title: 'How to change background color of a button', body: 'Give the button a class name. Create styles file. Put class name to button. Make background color to property of your choice. Link styles file in index file.', author: 'bob', id: 2},
+        { title: 'How to create index file', body: 'Open project in vs code. Click on new file. Name the file index.html. Put markup language in it. Start live server.', author: 'laquisha', id: 3}
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Home Page here!</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author } </p>
+                </div>
+            ))}
         </div>
         
      );
