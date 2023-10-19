9062a94d215ea2f12ce16ea9db64678de4a7c06b
llcain <llcain2012@gmail.com>
2023-10-19 11:43:54 -0500

    Created data folder - added db.json file

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'Create React App', body: 'Create a folder by opening bash terminal. Give the folder a name. Open in VS code. Open ternal and run npm create-react-app with dot afterwards. Press enter', author: 'laquisha', id: 1},
-        { title: 'How to change background color of a button', body: 'Give the button a class name. Create styles file. Put class name to button. Make background color to property of your choice. Link styles file in index file.', author: 'bob', id: 2},
-        { title: 'How to create index file', body: 'Open project in vs code. Click on new file. Name the file index.html. Put markup language in it. Start live server.', author: 'laquisha', id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // }
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data)
+            });
+    }, [blogs]);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
         
      );
