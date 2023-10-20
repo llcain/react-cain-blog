4e693178fd9186176bda9af83a0f3c4d1b9ff296
llcain <llcain2012@gmail.com>
2023-10-20 12:30:17 -0500

    Added delete button to blog details and styled button

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,34 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
-  const [author, setAuthor] = useState('mario');
+  const [author, setAuthor] = useState('');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+      history.push('/');
+    })
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input 
           type="text" 
@@ -30,7 +50,8 @@ const Create = () => {
           <option value="LaQuisha">LaQuisha</option>
           <option value="Codedreamer">Codedreamer</option>
         </select>
-        <button>Add Blog</button>
+        { !isPending && <button>Add Blog</button>}
+        { isPending && <button disabled>Add Blog...</button>}
       </form>
     </div>
   );
